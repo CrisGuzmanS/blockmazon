1a4e6d0fbe71ad94a5e11a12bb1a98b092870ceb
Personal <cristian@server.tech>
2020-06-08 22:35:04 -0500

    Trabajando en el calculo de las calificaciones del blockchain

--- app/views.py
+++ app/views.py
@@ -6,12 +6,42 @@ from flask import render_template, redirect, request
 
 from app import app
 
+
 # The node with which our application interacts, there can be multiple
 # such nodes as well.
 CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
 
 posts = []
+#projects = ["Uno", "Dos", "Tres"]
 
+class Project:
+
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+
+    def getRates(self):
+        pass
+        #get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+        #response = requests.get(get_chain_address)
+        #if response.status_code == 200:
+        #    content = []
+        #    chain = json.loads(response.content)
+        #    for block in chain["chain"]:
+        #        for tx in block["transactions"]:
+        #            tx["index"] = block["index"]
+        #            tx["hash"] = block["previous_hash"]
+        #            content.append(tx)
+
+        #    global posts
+        #    posts = sorted(content, key=lambda k: k['timestamp'],
+        #                reverse=True)
+
+projects = [
+    Project("Uno", "Erik"),
+    Project("Dos", "Brenda"),
+    Project("Tres", "Bryan")
+]
 
 def fetch_posts():
     """
@@ -37,9 +67,14 @@ def fetch_posts():
 @app.route('/')
 def index():
     fetch_posts()
+
+    for project in projects:
+        project.getRates()
+
     return render_template('index.html',
-                           title='A permissioned blockchain for rating sellers',
+                           title='Califica los proyectos :D',
                            posts=posts,
+                           projects=projects,
                            node_address=CONNECTED_NODE_ADDRESS,
                            readable_time=timestamp_to_string)
 
@@ -49,15 +84,18 @@ def submit_textarea():
     """
     Endpoint to create a new transaction via our application.
     """
-    post_content = request.form["content"]
-    author = request.form["author"]
-    response_time_score = request.form["response_time_score"]
+    failures_rate = request.form["failures"]
+    response_time_rate = request.form["response_time"]
+    view_rate = request.form["view"]
+
+
 
     post_object = {
-        'author': author,
-        'content': post_content,
-        'response_time_score': response_time_score,
+        'failures_rate': failures_rate,
+        'response_time_rate': response_time_rate,
+        'view_rate': view_rate,
     }
+
 
     # Submit a transaction
     new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
--- node_server.py
+++ node_server.py
@@ -151,7 +151,7 @@ peers = set()
 @app.route('/new_transaction', methods=['POST'])
 def new_transaction():
     tx_data = request.get_json()
-    required_fields = ["author", "content", "response_time_score"]
+    required_fields = ["failures_rate", "response_time_rate", "view_rate"]
 
     for field in required_fields:
         if not tx_data.get(field):
